8bf649c5fd2ac072e30bd1d1b36548e93e6a69ef
yeetie-mcyeetyote <42422389+yeetie-mcyeetyote@users.noreply.github.com>
2018-08-15 12:12:33 -1000

    Add files via upload

---
+++ arraybs.js
@@ -0,0 +1,25 @@
+var first = [
+
+];
+var last = [
+
+];
+
+//ask user for name
+
+var firstName = prompt("what is your first name?")
+
+first.push(firstName);
+
+var lastName = prompt("what is your last name?")
+
+last.push(lastName);
+
+//this tells what is displayed
+
+var cyka = first[0] + " " + last[0] + " " + first.length + " and " + last.length;
+
+//this a pos
+
+window.alert(cyka);
+
